10807eb23db5f0314d86dbf06045f26fa9ed40f3
kq <kongq1983@yeah.net>
2020-05-28 22:29:00 +0800

    add threadpoolexecutor

---
+++ src/main/java/com/kq/concurrent/executor/ThreadPoolExecutorShutdownDemo.java
@@ -0,0 +1,50 @@
+package com.kq.concurrent.executor;
+
+import com.kq.concurrent.util.DateUtil;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by qikong on 2020/5/28.
+ */
+public class ThreadPoolExecutorShutdownDemo {
+
+    static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3,5,
+            5, TimeUnit.SECONDS,new ArrayBlockingQueue<Runnable>(10));
+
+    public static void main(String[] args) {
+
+        AtomicInteger atomicInteger = new AtomicInteger(1);
+
+        Runnable runnable = ()-> {
+            System.out.println(Thread.currentThread().getName()+" is run.");
+
+            try {
+                TimeUnit.SECONDS.sleep(atomicInteger.incrementAndGet());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            System.out.println(DateUtil.getNowTime()+", "+Thread.currentThread().getName()+" is end.");
+
+        };
+
+        System.out.println(DateUtil.getNowTime()+", "+Thread.currentThread().getName()+" is start.===========");
+
+        for(int i=0;i<10;i++) {
+            threadPoolExecutor.execute(runnable);
+        }
+
+        threadPoolExecutor.shutdown();
+
+        System.out.println(DateUtil.getNowTime()+", "+Thread.currentThread().getName()+" is end.===========");
+
+
+
+    }
+
+
+}
