01449780255e4332653dac79673432ee5f6b3908
king <king@qq.com>
2020-06-03 17:12:40 +0800

    add demo

---
+++ src/main/java/com/kq/concurrent/jol/JOLSample_12_ThinLocking.java
@@ -0,0 +1,65 @@
+package com.kq.concurrent.jol;
+
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.vm.VM;
+
+import static java.lang.System.out;
+
+/**
+ * @author Aleksey Shipilev
+ */
+public class JOLSample_12_ThinLocking {
+
+    /*
+     * This is another dive into the mark word.
+     *
+     * Among other things, mark words store locking information.
+     * We can clearly see how the mark word contents change when
+     * we acquire the lock, and release it subsequently.
+     *
+     * This one is the example of thin (displaced) lock. The data
+     * in mark word when lock is acquired is the reference to the
+     * displaced object header, allocated on stack. Once we leave
+     * the lock, the displaced header is discarded, and mark word
+     * is reverted to the default value.
+     */
+
+    public static void main(String[] args) throws Exception {
+        out.println(VM.current().details());
+
+        final A a = new A();
+
+
+
+        ClassLayout layout = ClassLayout.parseInstance(a);
+
+        out.println("**** Fresh object");
+        out.println(layout.toPrintable());
+        out.println("a hashCode: " + Integer.toHexString(a.hashCode()));
+        out.println("before lock , main thread. hashCode: " + Integer.toHexString(Thread.currentThread().hashCode()));
+        out.println("before lock , main thread. id: " + Thread.currentThread().getId());
+        out.println("before lock , main thread. bi hashCode: " + Integer.toBinaryString(Thread.currentThread().hashCode()));
+
+        synchronized (a) {
+            out.println("**** With the lock");
+            out.println("a hashCode: " + Integer.toBinaryString(a.hashCode()));
+            out.println("locked main thread. hashCode: " + Integer.toHexString(Thread.currentThread().hashCode()));
+            out.println("locked main thread. id: " + Thread.currentThread().getId());
+            out.println("locked main thread. bi hashCode: " + Integer.toBinaryString(Thread.currentThread().hashCode()));
+            out.println(layout.toPrintable());
+        }
+
+        out.println("**** After the lock");
+        out.println("a hashCode: " + Integer.toHexString(a.hashCode()));
+        out.println("after lock , main thread. hashCode: " + Integer.toHexString(Thread.currentThread().hashCode()));
+        out.println("after lock , main thread. id: " + Thread.currentThread().getId());
+        out.println("after lock , main thread. bi hashCode: " + Integer.toBinaryString(Thread.currentThread().hashCode()));
+
+        out.println(layout.toPrintable());
+    }
+
+    public static class A {
+        // no fields
+    }
+
+}
---
+++ src/main/java/com/kq/concurrent/jol/JOLSample_13_BiasedLocking.java
@@ -0,0 +1,52 @@
+package com.kq.concurrent.jol;
+
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.vm.VM;
+
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.System.out;
+
+/**
+ * https://hg.openjdk.java.net/code-tools/jol/file/56dbba3b2c20/jol-samples/src/main/java/org/openjdk/jol/samples/JOLSample_13_BiasedLocking.java
+ * @author Aleksey Shipilev
+ */
+public class JOLSample_13_BiasedLocking {
+
+    /*
+     * This is the example of biased locking.
+     *
+     * In order to demonstrate this, we first need to sleep for >5 seconds
+     * to pass the grace period of biased locking. Then, we do the same
+     * trick as the example before. You may notice that the mark word
+     * had not changed after the lock was released. That is because
+     * the mark word now contains the reference to the thread this object
+     * was biased to.
+     */
+
+    public static void main(String[] args) throws Exception {
+        out.println(VM.current().details());
+
+        TimeUnit.SECONDS.sleep(6);
+
+        final A a = new A();
+
+        ClassLayout layout = ClassLayout.parseInstance(a);
+
+        out.println("**** Fresh object");
+        out.println(layout.toPrintable());
+
+        synchronized (a) {
+            out.println("**** With the lock");
+            out.println(layout.toPrintable());
+        }
+
+        out.println("**** After the lock");
+        out.println(layout.toPrintable());
+    }
+
+    public static class A {
+        // no fields
+    }
+
+}
